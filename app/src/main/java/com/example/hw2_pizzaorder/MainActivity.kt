b9dfa3525d58a2d71677e0002394bc9008d48ad3
AdrianAlucard <wartinator@gmail.com>
2020-02-12 15:18:37 -0500

    added event listeners

--- app/src/main/java/com/example/hw2_pizzaorder/MainActivity.kt
+++ app/src/main/java/com/example/hw2_pizzaorder/MainActivity.kt
@@ -2,15 +2,56 @@ package com.example.hw2_pizzaorder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.Switch
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.roundToInt
+import kotlin.math.roundToLong
 
 class MainActivity : AppCompatActivity() {
-    private var total: Number = 9.99
+    private var total: Double = 0.0
+    private var extras: Double = 0.0
+    private var pizzaCost: Double = 9.99
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setPizzaSelectListener()
+        calcTotal()
+    }
+
+    fun pizzaSizeClick(view: View) {
+        pizzaCost = when(view.id){
+            R.id.medium -> 9.99
+            R.id.large -> 13.99
+            else -> 15.99
+        }
+        calcTotal()
+    }
+
+    fun deliveryClick(view: View) {
+        val switch = findViewById<Switch>(view.id)
+        when(switch.isChecked) {
+            true -> extras+=2
+            else -> extras-=2
+        }
+        calcTotal()
+    }
+
+    fun extraToppingsClick(view: View) {
+        val checkBox = findViewById<CheckBox>(view.id)
+        when(checkBox.isChecked) {
+            true -> extras+=1.69
+            else -> extras-=1.69
+        }
+        calcTotal()
+    }
+
+    private fun calcTotal() {
+        total = pizzaCost + extras
+        total_text.text = "Total: $${String.format("%.2f", total)}"
     }
 
     private fun setPizzaSelectListener() {
